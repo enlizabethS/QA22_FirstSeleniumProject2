d98b31aa62bb6df65c362d440dd33d55e460de41
endorphinchik <elisaveta.stepura@mail.ru>
2023-03-27 21:55:22 +0300

    Qa commit

--- FirstSeleniumProject3/src/test/java/com/ait/qa22/FirstSeleniumTest.java
+++ FirstSeleniumProject3/src/test/java/com/ait/qa22/FirstSeleniumTest.java
@@ -1,11 +1,15 @@
 package com.ait.qa22;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class FirstSeleniumTest {
     WebDriver driver;
@@ -13,9 +17,15 @@ public class FirstSeleniumTest {
     //before - setUp(){}
     @BeforeMethod
     public void setUp() {
-         driver = new FirefoxDriver();
+//         driver = new FirefoxDriver();
 ////        driver = new SafariDriver();
-        driver.get("https://www.google.com");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+//        driver.get("https://www.google.com");
+        driver.navigate().to("https://www.google.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     //test
@@ -25,8 +35,9 @@ public class FirstSeleniumTest {
     }
 
     //after - tearDown(){}
-//    @AfterMethod
-//    public void tearDown(){
-//
-//    }
+ @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.quit();  // ---> close all tab
+//        driver.close(); --> close only one tab
+ }
 }
